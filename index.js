7c78c22c02c243fc28e8b9a62b819e49c5b402e9
kms06 <kms0644804@naver.com>
2020-08-18 09:27:40 +0900

    db 내용 분리

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 var express = require('express');
-var mongoose = require('mongoose');   // export MONGO_DB='mongodb+srv://testid:gangmin484@cluster-test.053ne.gcp.mongodb.net/test?retryWrites=true&w=majority'
 var bodyParser = require('body-parser');
 var app = express();
-
-// DB setting
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-console.log(process.env.MONGO_DB);
-mongoose.connect(process.env.MONGO_DB);
-var db = mongoose.connection;
-
-db.once('open', function(){
-  console.log('DB connected');
-});
-
-db.on('error', function(err){
-  console.log('DB ERROR : ', err);
-});
+var db = require('./db.js')
 
 // Other settings
 app.set('view engine', 'ejs');
@@ -26,36 +9,27 @@ app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json()); // 2
 app.use(bodyParser.urlencoded({extended:true}));
 
-// DB schema
-var contactSchema = mongoose.Schema({
-  name:{type:String, required:true, unique:true},
-  email:{type:String},
-  phone:{type:String}
-});
-var Contact = mongoose.model('contact', contactSchema);
-
 app.get('/', function(req, res){
   res.redirect('/contacts');
 });
 
 app.get('/contacts', function(req, res){
-  Contact.find({}, function(err, contacts){
+  db.Contact().find({}, function(err, contacts){
     if(err) return res.json(err);
     res.render('contacts/index', {contacts:contacts});
+  });
+});
+
+app.post('/contacts', function(req, res){
+  db.Contact().create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
   });
 });
 
 app.get('/contacts/new', function(req, res){
   res.render('contacts/new');
 });
-
-app.post('/contacts', function(req, res){
-  Contact.create(req.body, function(err, contact){
-    if(err) return res.json(err);
-    res.redirect('/contacts');
-  });
-});
-
 
 var port = 3000;
 app.listen(port, function(){
